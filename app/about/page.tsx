2d1df1e63c0a207b383d623ce000c6b541d9f883
RyoRyan <98072816+RyoRyan@users.noreply.github.com>
2026-03-25 21:09:00 +0900

    fourth commit?

---
+++ app/about/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <section>
+        <div className="font-bold text-center p-4">
+          <h1>五反田音波技研について</h1>
+        </div>
+      </section>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,75 @@
 import Image from "next/image";
 
+type News = {
+  id: string;
+  title: string;
+  category: {
+    name: string;
+  };
+  publishedAt: string;
+  createdAt: string;
+};
+
+const data: {
+  contents: News[];
+} = {
+  contents: [
+    {
+      id: "1",
+      title: "動画を公開しました",
+      category: {
+        name: "更新情報",
+      },
+      publishedAt: "2026/04/01",
+      createdAt: "2026/04/01",
+    },
+    {
+      id: "2",
+      title: "ツールを公開しました",
+      category: {
+        name: "更新情報",
+      },
+      publishedAt: "2026/04/01",
+      createdAt: "2026/04/01",
+    },
+    {
+      id: "3",
+      title: "動画を公開しました",
+      category: {
+        name: "更新情報",
+      },
+      publishedAt: "2026/04/01",
+      createdAt: "2026/04/01",
+    },
+  ],
+};
+
 export default function Home() {
+  const sliceData = data.contents.slice(0, 2);
+
   return (
-    <section>
-      <div>
-        <h1>五反田音響波動技術研究所</h1>;
-      </div>
-      <Image
-        src="/Screenshot 2026-02-16 at 3.49.03 PM.png"
-        alt=""
-        width={4000}
-        height={1200}
-      />
-    </section>
+    <>
+      <section>
+        <div className="font-bold text-center p-4">
+          <h1>五反田音響波動技術研究所</h1>
+        </div>
+      </section>
+      <section className="news">
+        <h2>News</h2>
+        <ul>
+          {sliceData.map((article) => (
+            <li key={article.id}>
+              <dl>
+                <dt className="newsItemTitle">{article.title}</dt>
+                <dd className="meta">
+                  <span className="tag">{article.category.name}</span>
+                  <span className="date">{article.publishedAt}</span>
+                </dd>
+              </dl>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </>
   );
 }
